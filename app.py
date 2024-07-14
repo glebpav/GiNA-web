6a5625c08e9f093697d0125a96b1ae9c2438e5c2
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-07-15 01:15:35 +0300

    beginning of refactoring

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ import json
 
 from flask import Flask
 from flask import request, Blueprint
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from api_config import CIRCULAR_GRAPH_URN, GRAPH_3D_URN
 from scripts.circular_graph_builder import build_circular_graph
 from scripts.graph_3d_builder import build_3d_graph
 
 
-
-g2d_bp = Blueprint('g2d', __name__, url_prefix=f'/{CIRCULAR_GRAPH_URN}')
-g3d_bp = Blueprint('g3d', __name__, url_prefix=f'/{GRAPH_3D_URN}')
+g2d_bp = Blueprint('g2d', __name__)
+g3d_bp = Blueprint('g3d', __name__)
 
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r'/*': {'origins': '*'}})
     
     app.register_blueprint(g2d_bp)
     app.register_blueprint(g3d_bp)
@@ -24,7 +23,7 @@ def create_app():
     return app
 
 
-@g2d_bp.post('/')
+@g2d_bp.post(f'/{CIRCULAR_GRAPH_URN}')
 def circular_graph():
     data = request.get_json()
     if isinstance(data, str):
@@ -36,7 +35,7 @@ def circular_graph():
     return response
 
 
-@g3d_bp.post('/')
+@g3d_bp.post(f'/{GRAPH_3D_URN}')
 def graph_3d():
     data = request.get_json()
     if isinstance(data, str):
--- scripts/graph_3d_builder.py
+++ scripts/graph_3d_builder.py
@@ -10,17 +10,7 @@ from naskit import *
 
 
 def find_centers(pos, loops):
-    centers = []
-    for l in loops:
-        coords = np.array([0., 0., 0.])
-        for idx in l:
-            coords += pos[idx]
-        centers.append(coords / len(l))
-
-    ! много написано. Нужные координаты можно взять листом индексов
-    ! centers.append(pos[l.nts].mean(0))
-    ! а дальше вообще в компрехеншн перевести - return [pos[l.nts].mean(0) for l in loops] - одна строка
-    return centers
+    return [pos[loop].mean(0) for loop in loops]
 
 
 def adjacency_matrix_to_list(adj_matrix):
@@ -42,37 +32,15 @@ def adjacency_matrix_to_list(adj_matrix):
 
 
 def get_loop_radius(target_len, points_number):
-    return target_len / (2 * math.sin(math.pi / (points_number)))
+    return target_len / (2 * math.sin(math.pi / points_number))
 
 
-def get_avg_connection_number(pos, A, loop_points_idxes):
-    ! это какое-то большое извращение. Если надо пробежаться по комплиментарным связям используй na.pairs и получишь пары индексов
-    ! число ковалентных связей в цепочке всегда одинаковое = N-1, где N - число нуклеотидов. Зачем тут делать каунтеры неясно
-    ! эта функция должна принимать na, координаты и содержать два маленьких цикла.
-    l0 = 0  # fuck(((((
-    connection_number = 0
-    not_loop_connection_len = 0
-    not_loop_connection_number = 0
-
-    for i in range(len(A)):
-        for point_idx in A[i]:
-            if i == point_idx:
-                continue
-
-            l0 += np.linalg.norm(pos[i] - pos[point_idx])
-            if i not in loop_points_idxes or point_idx not in loop_points_idxes:
-                not_loop_connection_len += l0
-                not_loop_connection_number += 1
-            connection_number += 1
-
-    l0 /= (connection_number)
-    not_loop_connection_len /= (not_loop_connection_number)
-
-    # print(f"{l0=}")
-    # print(f"{not_loop_connection_len=}")
-
-    return l0, not_loop_connection_len
-
+def get_avg_connection_number(pos: np.ndarray, na: NA, loop_points_idxes):
+    return np.mean(np.array([
+        np.linalg.norm(pos[idx1] - pos[idx2])
+        for (idx1, idx2) in na.pairs
+        if idx1 in loop_points_idxes and idx2 in loop_points_idxes
+    ]))
 
 def optimize(
         na: NA,
@@ -86,7 +54,7 @@ def optimize(
         iterations=50,
         add_counters_node=True
 ):
-    ! дальше нужно работать с матрицами, никаких циклов по листам
+    """! дальше нужно работать с матрицами, никаких циклов по листам"""
     A = adjacency_matrix_to_list(na.get_adjacency())
     A_origin = adjacency_matrix_to_list(na.get_adjacency())
 
@@ -101,18 +69,16 @@ def optimize(
     for loop in loops:
         loop_points_idxes += loop
 
-    ! громоздкая структура
-    loop_points_idxes = []
-    for l in na.loops:
-        loop_points_idxes.extend(l.nts)
+    loop_points_idxes = [nt for loop in na.loops for nt in loop.nts]
 
-    l0, not_loop_connection = get_avg_connection_number(pos, A, loop_points_idxes)
+    l0 = get_avg_connection_number(pos, na, loop_points_idxes)
+    # l01 = get_avg_connection_number1(pos=pos, na=na)
 
     pos /= l0
     l0 = 1
 
     x, y, z = zip(*pos)
-    ! зачем тут глобалка? она даже больше нигде не используется в файле
+    """! зачем тут глобалка? она даже больше нигде не используется в файле"""
     global max_value
     max_value = max(x + y + z, key=abs)
 
@@ -137,7 +103,7 @@ def optimize(
                 # print("i > len(A_origin)")
                 repulsion_mask[i][j] = 20
 
-    ! про итерирование по хеликсу уже написал
+    """! про итерирование по хеликсу уже написал"""
     for helix in [list(helix) for helix in na.helixes]:
         for idx, (i, j) in enumerate(helix):
             target_edge_ratios[i, j] = helix_ratio
@@ -156,44 +122,44 @@ def optimize(
 
         displacement *= 0
         centers = find_centers(pos, loops)
-        l0, not_loop_connection = get_avg_connection_number(pos, A, loop_points_idxes)
+        l0 = get_avg_connection_number(pos, na, loop_points_idxes)
 
         repulsion_force *= 0
 
-        ! этот цикл приемлем, ок
+        """! этот цикл приемлем, ок"""
         for center_idx, center in enumerate(centers):
             # r0 = get_loop_radius(not_loop_connection, len(loops[center_idx]))
             r0 = get_loop_radius(l0, len(loops[center_idx]))
             # print(f"{center_idx} - {r0=} - {center=}")
 
-            ! Этого цикла быть не должно
-            ! Ты должен взять матрицу координат петли pos[loop.nts] и работать с каждой точкой одновременно
+            """! Этого цикла быть не должно
+            ! Ты должен взять матрицу координат петли pos[loop.nts] и работать с каждой точкой одновременно"""
             for point_idx in loops[center_idx]:
                 delta = pos[point_idx] - center
                 distance = np.linalg.norm(delta)
-                ! зачем использовать такую усложненную конструкцию чтобы клипнуть значение?
-                ! max(0.001, distance) сделает тоже самое но в 100 раз понятнее
-                distance = np.where(distance < 0.001, 0.001, distance)
+                """! зачем использовать такую усложненную конструкцию чтобы клипнуть значение?
+                ! max(0.001, distance) сделает тоже самое но в 100 раз понятнее"""
+                """distance = np.where(distance < 0.001, 0.001, distance)"""
 
-                ! значения сил потом можно вставить в конкретный участок матрицы, т.к. loop.nts выдает индексы по порядку
+                """! значения сил потом можно вставить в конкретный участок матрицы, т.к. loop.nts выдает индексы по порядку
                 ! center_force[loop.nts[0]:loop.nts[-1]] = посчитанные силы
-                ! loop.nts надо сохранить отдельно, т.к. это проперти не кешируется в naskit
+                ! loop.nts надо сохранить отдельно, т.к. это проперти не кешируется в naskit"""
                 center_force[point_idx] = (-2) * (1 - r0 / distance) * delta
 
         # print(f"{center_force=}")
 
-        ! это цикл по всем нуклеотидам в цепочке и по их соседям - слишком долго
-        ! нужно сделать через матрицы сразу на все точки
+        """! это цикл по всем нуклеотидам в цепочке и по их соседям - слишком долго
+        ! нужно сделать через матрицы сразу на все точки"""
         
         for i in range(len(A)):
 
             elastic_force *= 0
             repulsion_force *= 0
 
-            ! попарные дистанции - np.linalg.norm(pos.reshape(-1, 1, 3) - pos, axis=-1)
+            """! попарные дистанции - np.linalg.norm(pos.reshape(-1, 1, 3) - pos, axis=-1)"""
             delta = (pos[i] - pos).T
             distance = np.sqrt((delta ** 2).sum(axis=0))
-            ! np.clip(distance, 0.001, np.inf)
+            """! np.clip(distance, 0.001, np.inf)"""
             distance = np.where(distance < 0.001, 0.001, distance)
 
             for idx1, connected_point_idx in enumerate(A[i]):
@@ -237,24 +203,18 @@ def compute_graph(sequence):
     G = nx.from_dict_of_lists(adj_list)
 
     knots_list = []
-    ! итерироваться можно прямо по хеликсу, не переводя в лист лишний раз
     for h in na.knot_helixes:
         for i, j in h:
-            ...
-    
-    for knot in [list(na.helixes[idx]) for idx in na.knots]:
-        for i, j in knot:
             knots_list.append(i)
             knots_list.append(j)
 
-    # print(f"Start spring layout for {sequence}")
-    ! число итераций должно подбираться в зависимости от размера структуры или числа связей в ней
+    """! число итераций должно подбираться в зависимости от размера структуры или числа связей в ней"""
     pos = nx.spring_layout(G, dim=3, iterations=5000)
     pos = np.array([value for value in pos.values()])
 
     # print(f"Start optimize for {sequence}")
-    ! число итераций должно подбираться в зависимости от размера структуры или числа связей в ней
-    ! 2000 это потолок
+    """! число итераций должно подбираться в зависимости от размера структуры или числа связей в ней
+    ! 2000 это потолок"""
     pos, adj_list = optimize(
         na,
         pos,
